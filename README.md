0eaa477d0c3b73e35214f8b315054e78bb2896ab
neha1304 <neha1304.v@gmail.com>
2021-06-14 12:33:56 +0530

    deleted all files and inserted python setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# IBM Cloud SQL Database
+This tutorial shows how to provision a SQL (relational) database service, create a table and load a larger data set, city information into the database. Thereafter, we deploy a web app "worldcities" to make use of that data and show how to access the cloud database. The app is written in Python using the Flask framework.
+
+This tutorial is part of [IBM Cloud tutorials](https://cloud.ibm.com/docs/tutorials?topic=solution-tutorials-tutorials) and discussed as [SQL Database for Cloud Data](https://cloud.ibm.com/docs/solution-tutorials?topic=solution-tutorials-sql-database).
+
+# Up and running in few steps
+To get this SQL database-backed app up and running only few steps and about 10 minutes are needed. Please follow the steps outlined in the IBM Cloud tutorial.
+ 
+# Feedback
+If you have feedback on the code or the related tutorial, please either open an issue on this repository or leave documentation feedback at the above mentioned tutorial.
---
+++ worldcities.py
@@ -0,0 +1,76 @@
+# (C) 2017 IBM
+# Author: Henrik Loeser
+#
+# Very short sample app used with Db2 Warehouse on Cloud to demonstrate
+# how to use a SQL Cloud Database with a web app.
+#
+
+import os
+from flask import Flask,redirect,render_template,request
+import urllib
+import datetime
+import json
+import ibm_db
+
+app = Flask(__name__)
+
+# get service information if on IBM Cloud Platform
+if 'VCAP_SERVICES' in os.environ:
+    db2info = json.loads(os.environ['VCAP_SERVICES'])['dashDB'][0]
+    db2cred = db2info["credentials"]
+    appenv = json.loads(os.environ['VCAP_APPLICATION'])
+else:
+    raise ValueError('Expected cloud environment')
+
+# handle database request and query city information
+def city(name=None):
+    # connect to DB2
+    rows=[]
+    try:
+      db2conn = ibm_db.connect(db2cred['ssldsn'], "","")
+      if db2conn:
+          # we have a Db2 connection, query the database
+          sql="select * from cities where name=? order by population desc"
+          # Note that for security reasons we are preparing the statement first,
+          # then bind the form input as value to the statement to replace the
+          # parameter marker.
+          stmt = ibm_db.prepare(db2conn,sql)
+          ibm_db.bind_param(stmt, 1, name)
+          ibm_db.execute(stmt)
+          # fetch the result
+          result = ibm_db.fetch_assoc(stmt)
+          while result != False:  # result is found
+              rows.append(result.copy())  #copy the result and append it to rows list
+              result = ibm_db.fetch_assoc(stmt)
+          # close database connection
+          ibm_db.close(db2conn)
+      return render_template('city.html', ci=rows) #passing the rows list (it contains result of query)
+    except:
+      app.logger.error('could not establish Db2 connection')
+      errorMsg = ibm_db.conn_errormsg()
+      app.logger.error(errorMsg)
+      return render_template('city.html', ci=[]) 
+
+
+# main page to dump some environment information
+@app.route('/')
+def index():
+   return render_template('index.html', app=appenv)
+
+# for testing purposes - use name in URI
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/search', methods=['GET'])
+def searchroute():
+    name = request.args.get('name', '')
+    return city(name)
+
+@app.route('/city/<name>')
+def cityroute(name=None):
+    return city(name)
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))
